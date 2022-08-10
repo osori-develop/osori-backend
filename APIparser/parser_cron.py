dba220b7e41d796e5d5dbe6fe9a9bc8c07e7ce26
kyw2271 <kyw2271@naver.com>
2022-08-10 23:26:42 +0900

    DB_mapping 작성

--- APIparser/parser_cron.py
+++ APIparser/parser_cron.py
@@ -1,2 +1,16 @@
 #testing.py
 #DB 인풋 및 test
+
+import parser.homepage_parser
+
+import DB_mapping
+
+
+def cron_day_menu() :
+
+    cbhs2 = DB_mapping.cbhs2_db()
+
+    #cbhs2.insert_menu()
+    
+    
+    return 0
